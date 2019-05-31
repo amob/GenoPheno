01801110ae4ecab26a3db7eb6ce4c26bf67ef20c
Anna O'Brien <amobrien@ucdavis.edu>
2019-05-31 17:45:30 -0400

    Update 009_selproj_diet.R

--- 009_selproj_diet.R
+++ 009_selproj_diet.R
@@ -58,7 +58,7 @@ tnames5 <- c("Days to flowering","Days to germination","Shoot biomass","Root bio
 
 ##popsoil
 #popsoil animal mods were iterated over 3, 5, 7 sample order of soil sites. but alphabetical order of plant pops
-Gnames.ps <- paste(rep(c("cu","cl","da","fp","m","mc","mt","tc","tx","tz"),times=3),".",rep(c("mc","mt","tc"),each=10),sep="")#alphabetical.
+Gnames.ps <- paste(rep(c("cl","cu","da","fp","m","mc","mt","tc","tx","tz"),times=3),".",rep(c("mt","mc","tc"),each=10),sep="")#alphabetical by pop but not soil
 
 #skewers
 #sel.Anpopsoil <- PhenSelection(An.popsoil,c(1,0,0,0,0,0,0,0,0))
@@ -69,7 +69,7 @@ sel.Anpopsoil.N5T <- PhenSelection(An.popsoil.N5T,c(1,0,0,0,0))#these are now on
 #plots
 rbd<-colorRampPalette(c(rgb(.8,0,0),rgb(.8,0,0,alpha=.5),rgb(1,1,1),rgb(0,0,0.8,alpha=.5),rgb(0,0,0.8)))
 
-Gnames.ps <- paste(rep(c("cu","cl","da","fp","m","mc","mt","tc","tx","tz"),times=3),".",rep(c("mt","mc","tc"),each=10),sep="")#represents actual order of Gs. 
+Gnames.ps <- paste(rep(c("cl","cu","da","fp","m","mc","mt","tc","tx","tz"),times=3),".",rep(c("mt","mc","tc"),each=10),sep="")#represents actual order of Gs. 
 #Gnames.ps useful for checking rearranging vectors to make sure they rearrange things as you think
 swapsoilpoptemp <- rep(c(10,5,9,7,3,4,6,1,8,2),times=3) + rep(c(0,10,20),each=10)#its going to fill by rows below, so we want first one soil, then the next then the last, almost as it is., pops in temp order
 AnPopsoilGs.5T <- apply(An.popsoil.5T,c(1,2,3),mean) [,,swapsoilpoptemp]#all three tz matrices first, all three cu last, rearranged to be soil treatment mc mt tc alphabetical
@@ -85,34 +85,36 @@ pdf("~/GenoPheno/cont2SelSkews3_5Trait.pdf",height=13,width=12)
 layout(matrix(1:16,ncol=4,byrow=T),heights=c(8,8,8,4),widths=c(4,8,8,8))
 par(mar=c(3,3,7,3))
 plot(NA,NA,bty="n",xlim=c(0,1),ylim=c(0,1),yaxt="n",xaxt="n")
-image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,12],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
+image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,2],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
 points(0,0)# now these will display properly
 axis(side=2, at=(1:5)-.5, labels=rev(tnames5), las=2, cex.axis = 2)
 mtext("Malinalco plants",side=3,line=1,cex=1.5)
 mtext("Biota15.0",side=3, line=3,adj=1.55,cex=1.5)
-image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,18],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
+image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,8],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
 points(0,0)# now these will display properly
 mtext("Lower Calimaya Plants",side=3,line=1,cex=1.5)
-kde11 <- kde(sel.Anpopsoil.5T[,c(1,4),11], H=bandwithmat, approx.cont=T)
-kde15 <- kde(sel.Anpopsoil.5T[,c(1,4),15], H=bandwithmat, approx.cont=T)
-plot(kde11,cont=c(50,80,90,95),col=contcols1[5:2],ylim=c(-1,1.5),xlim=c(-0.2,2),drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,0,0,alpha=.1))
-plot(kde15,cont=c(50,80,90,95),col=contcols2[5:2],add=T,drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,1,0,alpha=.1))
+kde1 <- kde(sel.Anpopsoil.5T[,c(1,4),1], H=bandwithmat, approx.cont=T)
+kde5 <- kde(sel.Anpopsoil.5T[,c(1,4),5], H=bandwithmat, approx.cont=T)
+plot(kde1,cont=c(50,80,90,95),col=contcols1[5:2],ylim=c(-1,1.5),xlim=c(-0.2,2),drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,0,0,alpha=.1))
+plot(kde5,cont=c(50,80,90,95),col=contcols2[5:2],add=T,drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,1,0,alpha=.1))
+abline(h=0)
 mtext("Root Mass Response",side=2,line=3,cex=1.5)
 mtext("Flowering Time Response",side=1,line=4,cex=1.5)
 mtext("Selection for later flowering",side=3,line=2,cex=1.4)#
 legend(-3.75,.75,c("Malinalco","Calimaya"),col=c(rgb(0,1,0),rgb(0,0,0)),pch=16,bty="n")
 
 plot(NA,NA,bty="n",xlim=c(0,1),ylim=c(0,1),yaxt="n",xaxt="n")
-image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,2],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
+image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,12],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
 axis(side=2, at=(1:5)-.5, labels=rev(tnames5), las=2, cex.axis = 2)
 mtext("Malinalco plants",side=3,line=1,cex=1.5)
 mtext("Biota14.3", side = 3,line=3,adj=1.55,cex=1.5)
-image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,8],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
+image(1:5,seq(from=.5,by=1, length.out=5),apply(AnPopsoilGs.5T[,,18],2,rev),ylim=c(0,5),zlim=c(-1,1),axes=F,ylab="",xlab="",col=rbd(101))
 mtext("Lower Calimaya Plants",side=3,line=1,cex=1.5)
-kde1 <- kde(sel.Anpopsoil.5T[,c(1,4),1],H=bandwithmat)
-kde5 <- kde(sel.Anpopsoil.5T[,c(1,4),5],H=bandwithmat)
-plot(kde1,cont=c(50,80,90,95),col=contcols1[5:2],ylim=c(-1,1.5),xlim=c(-0.2,2),drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,0,0,alpha=.1))
-plot(kde5,cont=c(50,80,90,95),col=contcols2[5:2],add=T,drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,1,0,alpha=.1))
+kde11 <- kde(sel.Anpopsoil.5T[,c(1,4),11],H=bandwithmat)
+kde15 <- kde(sel.Anpopsoil.5T[,c(1,4),15],H=bandwithmat)
+plot(kde11,cont=c(50,80,90,95),col=contcols1[5:2],ylim=c(-1,1.5),xlim=c(-0.2,2),drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,0,0,alpha=.1))
+plot(kde15,cont=c(50,80,90,95),col=contcols2[5:2],add=T,drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,1,0,alpha=.1))
+abline(h=0)
 mtext("Root Mass Response",side=2,line=3,cex=1.5)
 mtext("Flowering Time Response",side=1,line=4,cex=1.5)
 
@@ -131,6 +133,7 @@ kde21 <- kde(sel.Anpopsoil.5T[,c(1,4),21], H=bandwithmat, approx.cont=T)
 kde25 <- kde(sel.Anpopsoil.5T[,c(1,4),25], H=bandwithmat, approx.cont=T)
 plot(kde21,cont=c(50,80,90,95),col=contcols1[5:2],ylim=c(-1,1.5),xlim=c(-0.2,2),drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,0,0,alpha=.1))
 plot(kde25,cont=c(50,80,90,95),col=contcols2[5:2],add=T,drawpoints=T,pch=16,cex=.5,col.pt=rgb(0,1,0,alpha=.1))
+abline(h=0)
 mtext("Root Mass Response",side=2,line=3,cex=1.5)
 mtext("Flowering Time Response",side=1,line=4,cex=1.5)
 legend(0.5,-0.3,c("Malinalco","Calimaya"),col=c(rgb(0,1,0),rgb(0,0,0)),lty=1,bty="n",cex=2)
@@ -142,7 +145,7 @@ contcols3 <- rgb(1,0,1,alpha=seq(from = 0,to=1,length.out=5))
 bandwithmat <- matrix(c(0.02,.008,.008,0.02),nrow=2)
 
 all.sel.contours <- lapply(1:30, function(z) kde(sel.Anpopsoil.5T[,c(1,4),z],H=bandwithmat,approx.cont=T))
-popmats <- matrix( rep(c(10,0,20),times=10) + rep(c(10,5,9,7,3,4,6,1,8,2),each=3), ncol=3,byrow=T) # all.sel.contours is alphabetical and needs to be in MAT order
+popmats <- matrix( rep(c(10,0,20),times=10) + rep(c(10,5,9,7,3,4,6,1,8,2),each=3), ncol=3,byrow=T) # all.sel.contours is alphabetical and soi is rearranged mc, mt, tc.
 popnamestemp <- c("Tepoztlán 19.8","Malinalco 18.6","Texcoco 15.3","San Mateo Tezoquipan 15.0","Tenango del Aire 14.7",
 					"San Francisco Pedregal 14.4","San Matías Cuijingo 14.3","Calimaya Lower 13.2","Toluca 13.0","Calimaya Upper 12.9") #CL CU DA FP M MC MT TC TX TZ
 pdf("~/GenoPheno/TESTsoil_sel_all.pdf",height=15,width=7)
@@ -159,6 +162,6 @@ for(i in 1:10){
 #	if(i==1){mtext("Selection for later flowering",side=3,line=2,cex=1.4)#}
 	if(i==5){mtext("Root Mass Response",side=2,line=3,cex=1.5)}
 	if(i==9){mtext("Flowering Time Response",side=1,line=4,cex=1.5,adj=-8)}
-	legend(-3.75,.75,c("Biota15.0","Biota14.3","Biota13.0"),col=c(rgb(0,0,0),rgb(0,1,0),rgb(0,0,1)),pch=16,bty="n")
+	legend(-3.75,.75,c("Biota15.0","Biota14.3","Biota13.0"),col=c(rgb(0,0,0),rgb(0,1,0),rgb(1,0,1)),pch=16,bty="n")
 }
 dev.off()
